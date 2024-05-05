d684d9ff88511b0216a5e9c40c4f72c8e00c9dd8
Prureddy <pruthvisreddy8861@gmail.com>
2024-05-05 18:27:17 +0530

    updating the resume

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -10,7 +10,8 @@ import React, { useState, useEffect } from "react";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Prureddy/React_portfolio/main/src/assets/Pruthvi_Resume.pdf";
+  "https://raw.githubusercontent.com/Prureddy/React_portfolio-main/main/src/assets/Pruthvi_Resume.pdf";
+  
 
 function Resume() {
   const [width, setWidth] = useState(1200);
